ebfbd8af0730b49d2993176443d8b70ca421dc82
krishna <92731672+krishnaju07@users.noreply.github.com>
2024-06-12 15:44:31 +0530

    Update index.js

--- hari/index.js
+++ hari/index.js
@@ -1,49 +1,4 @@
-let arr =['bound','unbound','unbound','bound','unbound','unbound','bound','bound']
 
-let number = 2
-
-let str = 'bound'
-
-function find(arr,num,string){
-    let count  = 0;
-    
-    for(let i =0;i <arr.length;i++){
-        if(arr[i] === string){
-            count++;
-            
-        }
-        
-        if(count === number){
-            return i
-        }
-        
-    }
-    
-}
-
-// const result = arr.indexOf('unbound')
-
-console.log(find(arr,number,str))
-
-
-//////
-
-let str = "aaabbcdddeeeea";
-
-function merge(str){
-let convert = str.split("");
-let res = ''
-for(let i =0;i<convert.length;i++){
-
-   let count = 1
-   while(convert[i] === convert[i+1]){
-       count++ ;
-       i++
-   }
-   res += convert[i] + count
-}
-    return res
-}
 // 1 ---> Fizz Buzz Implementation
 
 // Qus : Given an integer n, for every integer i <= n, the task is to print “FizzBuzz” if i is divisible by 3 and 5, “Fizz” if i is divisible by 3, “Buzz” if i is divisible by 5 or i (as a string) if none of the conditions are true.
