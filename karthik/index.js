7eba637d9515edee190f66172a857f5428d0ee27
karti16 <kartim316@gmail.com>
2024-06-22 18:22:59 +0530

    q

--- karthik/index.js
+++ karthik/index.js
@@ -143,3 +143,33 @@ minBy([{ n: 1 }, { n: 2 }], (o) => o.m); // => undefined
 
 
 /* ============================================== ============================================== ==============================================*/
+
+
+/* 
+Implement a function that takes one or more values and returns a function that cycles 
+through those values each time it is called.
+
+const helloFn = cycle('hello');
+console.log(helloFn()); // "hello"
+console.log(helloFn()); // "hello"
+
+const onOffFn = cycle('on', 'off');
+console.log(onOffFn()); // "on"
+console.log(onOffFn()); // "off"
+console.log(onOffFn()); // "on"
+ */
+
+// function cycle(...values) {
+//   return () => {
+//     const top = values.shift();
+//     values.push(top);
+//     return top;
+//   }
+// }
+
+// const onOffFn = cycle('on', 'off');
+// console.log(onOffFn()); // "on"
+// console.log(onOffFn()); // "off"
+// console.log(onOffFn()); // "on"
+
+/* ============================================== ============================================== ==============================================*/
