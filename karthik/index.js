e4e07fe3eec1872e7911480c169e6a73c11d6428
karti16 <kartim316@gmail.com>
2024-06-22 16:52:26 +0530

    q2

--- karthik/index.js
+++ karthik/index.js
@@ -26,8 +26,7 @@ counter(); // 7
 // counter(); // 1
 // counter(); // 2
 
-/* ============================================== */
-
+/* =============================================== ============================================= ==============================================*/
 
 /* mplement a function mean(array) that returns the mean (also known as average) 
 of the values inside array, which is an array of numbers. 
@@ -44,4 +43,40 @@ mean([1, 2, 2]); // => 1.6666666666666667
 
 // console.log(mean([1, 2, 3, 4]));
 
-/* ============================================== */
+/* ============================================== ============================================== ==============================================*/
+
+/*  
+Implement a function minBy(array, iteratee) that finds the element inside array with the minimum value after going through iteratee.
+Arguments
+
+    array (Array): The array to iterate over.
+    iteratee (Function): The iteratee invoked per element, which is a function that accepts one argument: (value).
+
+
+minBy([2, 3, 1, 4], (num) => num); // => 1
+minBy([{ n: 1 }, { n: 2 }], (o) => o.n); // => { n: 1 }
+
+The function should ignore elements where iteratee produces null or undefined.
+minBy([{ n: 1 }, { n: 2 }], (o) => o.m); // => undefined
+
+*/
+
+function minBy(array, iteratee) {
+  let computed;
+  let result;
+
+  for (const val of array) {
+    let output = iteratee(val);
+
+    if (output != null && (computed === undefined || output < computed)) {
+      computed = output;
+      result = val;
+    }
+  }
+
+  return result;
+}
+
+console.log(minBy([2, 3, 1, 4], (num) => num));
+console.log(minBy([{ n: 1 }, { n: 2 }], (o) => o.n));
+console.log(minBy([{ n: 1 }, { n: 2 }], (o) => o.m));
