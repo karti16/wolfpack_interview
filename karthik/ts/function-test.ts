a99d4b1ed05242dfbe47efa3a0dac1d5c0464a47
karti16 <kartim316@gmail.com>
2024-06-25 00:23:45 +0530

    ts

--- karthik/ts/function-test.ts
+++ karthik/ts/function-test.ts
@@ -1,3 +1,4 @@
+import { printToFile } from './funcs-and-funcs';
 import addNumber, { addString, getName } from './function';
 
 console.log(addNumber(2, 3));
@@ -7,3 +8,5 @@ console.log(addString('sdsds'));
 
 console.log(getName({ first: 'karthik', last: 'm' }));
 console.log(void 0);
+
+console.log(printToFile);
