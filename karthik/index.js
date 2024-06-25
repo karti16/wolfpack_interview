872d4970db5e2e74b7994091a9616b284b5b6a6c
karti16 <kartim316@gmail.com>
2024-06-25 23:41:47 +0530

    new problem

--- karthik/index.js
+++ karthik/index.js
@@ -1,5 +1,61 @@
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane User";
-document.body.textContent = greeter(user);
+// const obj = {}
+
+// obj['a'] = 'aaa';
+// obj['c'] = 'ccc';
+// obj['b'] = 'bbb';
+
+// console.log(obj); // { a: 'aaa', c: 'ccc', b: 'bbb' }
+
+// ----------------------------------------
+
+// const obj1 = {
+//     '45':'4545',
+//     '1': '1010',
+//     '5':'454',
+//     '111':'1111'
+// }
+
+// obj1['5'] = '555';
+// obj1['2'] = '222';
+// obj1['1'] = '111';
+
+// console.log(obj1); // { '1': '111', '2': '222', '5': '555' }
+
+
+
+// function sortByNumber(str) {
+//   if (str === '') return str;
+  
+//   const arr = str.split(' ');
+//   const obj = {};
+
+//   for(let ele of arr){
+//     const num = ele.split('').filter(i => !isNaN(i))
+//     console.log(num);
+//     obj[num] = ele;
+//   }
+
+//   return Object.values(obj).join(' ');
+// }
+
+// console.log(sortByNumber('4of Fo1r pe6ople g3ood th5e the2'));
+
+// function print() {
+//   console.log('tick...');
+// }
+
+// const throttledPrint = throttle(print, 3000);
+
+// setInterval(throttledPrint, 1000);
+
+// function throttle(fn, delay) {
+//   let timer = null;
+//   return () => {
+//     if (timer === null) {
+//       fn();
+//       timer = setTimeout(() => {
+//         timer = null;
+//       }, delay);
+//     }
+//   };
+// }
---
+++ karthik/ts/optional.ts
@@ -0,0 +1,5 @@
+function printIngredient(quantity: number, ingredient: string) {
+  console.log(`${quantity} ${ingredient}`);
+}
+
+printIngredient(12, 'flour');
